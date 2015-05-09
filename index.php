fc28654db3eb11becb01901745282cc70afdab7a
ampmail <bounty.mail@gmail.com>
2015-05-09 17:29:11 +0300

    test

--- index.php
+++ index.php
@@ -2,8 +2,37 @@
 <html>
 <head>
     <meta charset=utf-8>
+    <link rel="stylesheet" href="main.css">
+
 </head>
 <body>
+
+<ul class="nav">
+    <li class="main-menu">
+        <a href>Main Menu</a>
+        <a href>Products</a>
+        <a href>Store</a>
+        <a href>Forum</a>
+    </li>
+    <li class="settings">
+        <a href>Settings</a>
+        <a href>Account</a>
+        <a href>Notifications</a>
+        <a href>Dislpay</a>
+    </li>
+    <li class="link">
+        <a href>Link</a>
+        <a href>Embed</a>
+        <a href>Short link</a>
+        <a href>Full link</a>
+    </li>
+    <li class="search">
+        <a href>Search</a>
+        <a href>Item 2</a>
+        <a href>Item 3</a>
+        <a href>Item 4</a>
+    </li>
+</ul>
 
 <?php
 class foo {
@@ -13,9 +42,6 @@ class foo {
 $foo = new foo();
 $bar = 'bar';
 $baz = array('foo', 'bar', 'baz', 'quux');
-echo "{$foo->$bar}\n";
-echo "{$foo->$baz[1]}\n";
+//echo "{$foo->$bar}\n";
+//echo "{$foo->$baz[1]}\n";
 ?>
-
-</body>
-</html>
