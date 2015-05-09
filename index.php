716d0ea1a74cb1448db65b405c1bf048236a544b
ampmail <bounty.mail@gmail.com>
2015-05-09 17:16:35 +0300

    test

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset=utf-8>
+</head>
+<body>
+
 <?php
-echo "Hello AMP 21!!!!";
+class foo {
+    var $bar = 'I am bar.';
+}
+
+$foo = new foo();
+$bar = 'bar';
+$baz = array('foo', 'bar', 'baz', 'quux');
+echo "{$foo->$bar}\n";
+echo "{$foo->$baz[1]}\n";
+?>
+
+</body>
+</html>
